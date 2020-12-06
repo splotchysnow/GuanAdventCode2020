5e9028b51879faac0c5b50008945ef9029544557
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2020-12-05 16:13:06 -0800

    Day 2 Part 2 complete

--- Day2/Password Philosophy part2.py
+++ Day2/Password Philosophy part2.py
@@ -2,7 +2,7 @@ def main():
     with open("Day2/puzzleInput.txt") as f:
         data = f.readlines()
     sentenceCounter = 0 #initalize the counter
-    iCounter = 0 #DEBUG: for debugging Purpose
+    # iCounter = 0 #DEBUG: for debugging Purpose
 
     #Slicing:
     for i in data:
@@ -27,60 +27,23 @@ def main():
         # print(sentence)
 
 
-        letterCounter = 0 # reset the count for the letter valid
-                
-        '''
-        OK SO here is the plan: 
-            Get J loop through I like last time, everytime J is found. get the position of J and store it in a temperary list
-            and then check through that list with the value and see if it matches firstValue and secondValue
-            If so GOOD. If not, Invalid
-        '''
-
-        # jCounter = 0 #reset counter
-        # jList = [] #empty the list
-        # for j in i:
-        #     if j == letter: #if the J letter matches the letter then get the position
-        #         jList.append(jCounter) #Store the position into the list
-        #     jCounter += 1 #Counter go up by 1
-        
-        # #here is where I check if the position excist in the jList:
-        # for z in jList:
-        #     if (z == firstValue + 1  or z == secondValue +1):
-        #         sentenceCounter += 1
-        
-
-        #         #DEBUG Can Restrict the amount of looping:        
-        # iCounter = iCounter + 1 #counting I
-        # # print(iCounter)
-
-        # if iCounter == 5:
-        #     break
-        #         #DEBUG:
-        # print(jList)
-
         check = False #Reset Check to false
-        string1 = i[firstValue] # get the first string position
-        string2 = i[secondValue] # get the second string posiiton
+        string1 = sentence[firstValue - 1] # get the first string position
+        string2 = sentence[secondValue - 1] # get the second string posiiton
 
-        if (string1 == letter): #If else statement for counting
-            check = True
-        elif (string2 == letter):
+        # print(string1)
+        # print(string2)
+        # print(letter)
+
+        if (string1 == letter and string2 == letter): #If else statement for counting
+            check = False
+        elif (string2 == letter or string1 == letter):
             check = True
         else:
             check = False
-        
         if check: #if true, count + 1
             sentenceCounter += 1
 
     print(sentenceCounter)
-
-
-
-        
-
-
-
-
-
 
 main()
