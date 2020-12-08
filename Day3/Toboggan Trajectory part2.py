da282610c208542034b3e43fb65240e32a920ec0
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2020-12-07 22:07:59 -0800

    Updated Passport Processing Part1

--- Day3/Toboggan Trajectory part2.py
+++ Day3/Toboggan Trajectory part2.py
@@ -15,23 +15,15 @@ def storeAsArray(data):
 def lengthPerLine(data):
     return len(data[0]) # will return the length of the first line in data
 
-
-def main():
-
+def slopeTreeCount( slopeX, slopeY, data):
     treeCounter = 0 #Count the amount of trees being hit.
-
-    data = openFile() #I open the file and "data" represent the file.
-    
     linesInArray = []
     linesInArray = storeAsArray(data) #store each line in a single array
-
     length = lengthPerLine(data) #length per each line:
     lineLength = len(linesInArray) 
-
     # print(length)
     #print(linesInArray)
     #print(lineLength)
-
 
     #initiating the coordinate
     x = 0
@@ -42,8 +34,8 @@ def main():
 
         if (linesInArray[y][x]) == "#":
             treeCounter += 1
-            print("Horizontal = ", x + 1 , "and Verticle = " , y + 1)
-            print("The Symbole is: ", linesInArray[y][x])   
+            # print("Horizontal = ", x + 1 , "and Verticle = " , y + 1)
+            # print("The Symbole is: ", linesInArray[y][x])   
         
         #Dubugging Purposes
         # print("Horizontal = ", x , "and Verticle = " , y)
@@ -51,22 +43,39 @@ def main():
         
         
         # Restrict the X's Boundaries.  If x is about to be > length then restrict it back to 0 # Move x to the right by 3
-        if ((x + 3) >= (length - 1)):
-            remainder = (x + 3) - (length - 1)
+        if ((x + slopeX) >= (length - 1)):
+            remainder = (x + slopeX) - (length - 1)
             x = 0 #reset x back to 0
             x += (remainder)
         else:
-            x += 3
+            x += slopeX
         
 
 
         #move y coordinate
-        y += 1 #move down by 1 line
+        y += slopeY #move down by 1 line
         
         #exit condition
-        if (y == lineLength):
+        if (y >= lineLength):
             break
-    print(treeCounter)
+    return treeCounter
+
+
+
+def main():
+
+    data = openFile() #I open the file and "data" represent the file.
+    ans1=slopeTreeCount(1,1,data) # Right 1, down 1.
+    ans2=slopeTreeCount(3,1,data) # Right 3, down 1. (This is the slope you already checked.)
+    ans3=slopeTreeCount(5,1,data) # Right 5, down 1.
+    ans4=slopeTreeCount(7,1,data) # Right 7, down 1.
+    ans5=slopeTreeCount(1,2,data) # Right 1, down 2.
+    print(ans1*ans2*ans3*ans4*ans5)
+
+  
+    
+    
+    
     
 
 if __name__ == "__main__":
--- Day4/Passport Processing Part1.py
+++ Day4/Passport Processing Part1.py
@@ -1,23 +1,83 @@
+import re #getting regular Expression
+from typing import List
+
+
 def openFile():
+    #Maybe for each time readlines == "\n" move it to a different spot to store.
     with open("Day4/puzzleInput.txt") as f:
         data = f.readlines()
+        # print(data)
         f.close()
+        
     return data
 
 #Count the total amount of people in the passport thing. Probably useless.
-def countPeople():
+
+def listSize(list_):
+    return len(list_) #return the size of the list
+
+
+def emptyLineCount(data):
     count = 0
-    data = openFile()
+    arrayCount = []
     for i in data:
+        count += 1
+        # print(i)
         if i == "\n":
-            count = count + 1
-    return count
+            arrayCount.append(count - 1) #put all the empty line's number in a single list
+    return arrayCount
+
+def assignPersonalInfo(list_, byr, iyr, eyr, hgt, hcl, ecl, pid, cid):
+    for i in list_:
+        for j in i: #2nd Dimentions, with all information of passport
+            pass
+
+#Display the information in a 2dm array with each person's info in the 2nd dimension
+def cutInfoByPerson(data, eLC):
+    previous = 0
+    list_ = []
+    for i in eLC:
+        current = i
+        list_.append(data[previous:current])
+        previous = current
+    return list_
+
+#Concavate the 2d array into a 1d array:
+def conList(personInfoList):
+    list_ = []
+    for i in personInfoList:     #concate all the string List into a long giant string
+        longString = "" #reset past into an empty string
+        for j in i:
+            longString += (j + " ") #concavate:
+        list_.append(longString)
+    return list_
 
 
 def main():
     data = openFile()
-    print(countPeople())
+    eLC = emptyLineCount(data)
+    personInfoList = cutInfoByPerson(data, eLC) #have the information of each person
+    personInfoList = conList(personInfoList) #Store everything into a single 1d Array
+
+    for i in personInfoList:
+        print(i.find("pid"))
+
+
+
+
+
+
+
     
+
+
+
+
+
+
+
+
+    # print(data)
 
 if __name__ == "__main__":
     main()
