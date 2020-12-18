6725bb7e85dacde87171f0a21a0372f17196df26
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2020-12-18 03:36:15 -0800

    Almost Finished with Day 4
    Set up all the funciton,
    Should be finished with the project once the functions are finished

--- Day4/Passport.py
+++ Day4/Passport.py
@@ -1,4 +1,4 @@
-#A class for Passport that contains different attributes.
+# A class for Passport that contains different attributes.
 
 class Passport:
 
@@ -12,32 +12,129 @@ class Passport:
         self.eyr = eyr
         self.hcl = hcl
         self.check = self.checkInvalidPassport()
-    
+
     def getEcl(self):
         return self.ecl
+
     def getByr(self):
         return self.byr
+
     def getIyr(self):
         return self.iyr
+
     def getPid(self):
         return self.pid
+
     def getCid(self):
         return self.cid
+
     def getHgt(self):
         return self.hgt
+
     def getEyr(self):
         return self.eyr
+
     def getHcl(self):
         return self.hcl
+
     def getCheck(self):
         return self.check
-    
+
     def checkInvalidPassport(self):
-        F = ""
-        if ((self.ecl != F) and (self.byr != F) and (self.iyr != F) and (self.pid != F) and (self.hgt != F) and (self.eyr != F) and (self.hcl != F)):
+        if (
+                self.existenceChecker() and self.byrChecker() and self.cidChecker() and self.eclChecker() and
+                self.eyrChecker() and self.hclChecker()
+                and self.hgtChecker() and self.iyrChecker() and self.pidChecker()):
             return True
         else:
             return False
 
+    # when called, check if the checker is working properly with correct examples.
+    def checkChecker(self):
+        error = []
+        if not self.existenceChecker():
+            error.append("Existence False")
+        if not self.byrChecker():
+            error.append("Byr False")
+        if not self.cidChecker():
+            error.append("cid false")
+        if not self.eclChecker():
+            error.append("Ecl false")
+        if not self.eyrChecker():
+            error.append("EYR FALSE")
+        if not self.hclChecker():
+            error.append("HCL False")
+        if not self.hgtChecker():
+            error.append("Hgt false")
+        if not self.iyrChecker():
+            error.append("IYR false")
+        if not self.pidChecker():
+            error.append("Pid false")
+        return error
 
-        
+    def existenceChecker(self):
+        F = ""
+        if ((self.ecl != F) and (self.byr != F) and (self.iyr != F) and (self.pid != F) and (self.hgt != F) and (
+                self.eyr != F) and (self.hcl != F)):
+            return True
+        else:
+            return False
+
+    # TODO: Not so sure that maybe length is 3 or 4... I think 4 is fine for now...
+    def byrChecker(self):
+        byr = int(self.byr)
+        if len(self.byr) == 4 and (1920 <= byr <= 2002):
+            return True
+        else:
+            return False
+
+    def iyrChecker(self):
+        iyr = int(self.iyr)
+        if len(self.iyr) == 4 and (2010 <= iyr <= 2020):
+            return True
+        else:
+            return False
+
+    def eyrChecker(self):
+        eyr = int(self.eyr)
+        if len(self.eyr) == 4 and (2020 <= eyr <= 2030):
+            return True
+        else:
+            return False
+
+    # Get the letter for if the thing is cm or inch.
+    # I Think this function is now working properly.
+    def hgtChecker(self):
+        hgt = int(self.hgt[:-2])
+        measurement = self.hgt[-2:len(self.hgt)] # Either in or cm
+        # print(hgt)
+        if measurement == "cm" and (150 <= hgt <= 193):
+            return True
+        elif measurement == "in" and (59 <= hgt <= 76):
+            return True
+        else:
+            return False
+
+    # TODO: Either get the character value and just compare the value, or make an arrya with 0-9 and a-f and check it that way.
+    def hclChecker(self):
+        # gotta find those values...
+
+        return True
+
+    # either amb, blu, brn, gry, grn, hzl, oth
+    def eclChecker(self):
+        ecl = self.ecl
+        arrayOfSolution = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+        for i in arrayOfSolution:
+            if i == ecl:
+                return True
+        return False
+
+    # TODO: a nine digit number including leading zeros.
+    # Maybe do a catch int error? LOL THAT IS A NICE IDEA ACTUALLY.
+    def pidChecker(self):
+        return True
+
+    # Ignored, just doesn't matter at all... don't even do it;
+    def cidChecker(self):
+        return True
---
+++ Day4/Passport_Processing.py
@@ -0,0 +1,148 @@
+# import re  # getting regular Expression
+from Passport import Passport
+# from typing import List, Sized
+
+
+# Open the file for the puzzle input and store the data and return the data
+def openFile():
+    # Maybe for each time headlines == "\n" move it to a different spot to store.
+    with open("puzzleInput.txt") as f:
+        data = f.readlines()
+        # print(data)
+        f.close()
+
+    return data
+
+
+# Count the total amount of people in the passport thing. Probably useless. (Actually its quite useful...)
+def listSize(list_):
+    return len(list_)  # return the size of the list
+
+
+# Count the amount of empty line inside the puzzle Input to segregate the information by people
+def emptyLineCount(data):
+    count = 0
+    arrayCount = []
+    for i in data:
+        count += 1
+        # print(i)
+        if i == "\n":
+            arrayCount.append(count - 1)  # put all the empty line's number in a single list
+    arrayCount.append(listSize(data) - 1)
+    return arrayCount
+
+
+# Display the information in a 2dm array with each person's info in the 2nd dimension
+def cutInfoByPerson(data, elc):
+    previous = 0
+    list_ = []
+    for i in elc:
+        current = i
+        list_.append(data[previous:current])
+        previous = current
+    return list_
+
+
+# Concave the 2d array into a 1d array:
+def conList(person_info_list):
+    list_ = []
+    for i in person_info_list:  # concat all the string List into a long giant string
+        longString = ""  # reset past into an empty string
+        for j in i:
+            longString += (j + " ")  # concave:
+        list_.append(longString)
+    return list_
+
+
+def infoList(list_, info="pid"):
+    pList = []
+    for i in list_:
+        pidIIndex = i.find(info)
+        temp = i[
+               pidIIndex:]  # let a temporary string = to from pid index to the end of string. find the first space
+        # or "\n"
+        pidFIndex = temp.find("\n")
+        if temp.find(" ") < temp.find("\n"):
+            pidFIndex = temp.find(" ")
+        pid = temp[4:pidFIndex]
+        pList.append(pid)
+    return pList
+
+
+def main():
+    # Declare data and lines counts. For the parameter.
+    data = openFile()
+    eLC = emptyLineCount(data)
+    personInfoList = cutInfoByPerson(data, eLC)  # have the information of each person
+    personInfoList = conList(personInfoList)  # Store everything into a single 1d Array
+
+    # Lists
+    eclList = infoList(personInfoList, "ecl")  # store all ecl
+    byrList = infoList(personInfoList, "byr")  # store all byr
+    iyrList = infoList(personInfoList, "iyr")  # store all iyr
+    pidList = infoList(personInfoList, "pid")  # store all pid
+    cidList = infoList(personInfoList, "cid")  # store all cid
+    hgtList = infoList(personInfoList, "hgt")  # store all hgt
+    eyrList = infoList(personInfoList, "eyr")  # store all eyr
+    hclList = infoList(personInfoList, "hcl")  # store all hcl
+
+    # Finding Size and Counting!
+    size = listSize(eclList)  # get the size of any list for the boundaries of the While Loop
+    count = 0  # initialize count to zero
+    # firstPerson = Passport(eclList[0], byrList[0], iyrList[0], pidList[0], cidList[0], hgtList[0], eyrList[0],
+    # hclList[0]) Creating an array of Passports:
+    passPortLine = []  # create an empty array of passports
+    while count < size:
+        passport = Passport(eclList[count], byrList[count], iyrList[count], pidList[count], cidList[count],
+                            hgtList[count], eyrList[count], hclList[count])
+        passPortLine.append(passport)
+        count = count + 1
+    validCount = 0
+    lineCounter = 0  # debug purpose
+    invalidList = []  # debug purpose
+    for i in passPortLine:
+        if i.checkInvalidPassport():
+            validCount += 1
+        if not i.checkInvalidPassport():
+            invalidList.append(lineCounter)
+        lineCounter += 1  # debugging purpose
+    # The Result!
+    print("The Count is:", validCount)
+    print(passPortLine[0].hgtChecker())
+    print(passPortLine[0].getHgt())
+
+
+def debug():
+    # print(passPortLine[2].check)
+    # print(passPortLine[0].)
+    # print(len(invalidList))
+    # print(passPortLine[0].existenceChecker())
+    # #print(passPortLine[0].checkInvalidPassport())
+    # print(passPortLine[0].byrChecker())
+    # print(passPortLine[0].cidChecker())
+    # print(passPortLine[0].eclChecker()) #return none
+    # print(passPortLine[0].eyrChecker())
+    # print(passPortLine[0].hclChecker())
+    # print(passPortLine[0].hgtChecker()) #return None
+    # print(passPortLine[0].iyrChecker())
+    # print(passPortLine[0].pidChecker()) #return None
+
+    # The answer is between 162 to 290
+
+    # #debug
+    # print(eLC)
+    # print(personInfoList)
+    # print(eclList[-1])
+    # print(byrList[-1])
+    # print(iyrList[0])
+    # print(pidList[0])
+    # print(cidList[0])
+    # print(hgtList[0])
+    # print(eyrList[0])
+    # print(hclList[0])
+    # print(data)
+    pass
+
+
+if __name__ == "__main__":
+    main()
